4d772b16f203264983d548ab9cb5b08f3e10941c
Milena valente <milena_frey@estudante.sesisenai.org.br>
2026-06-10 21:50:31 -0300

    par e inpar

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Hello, World!");
+// cria a variavel variavelQueSalvaUmnumero,
+// leia o que o usário digitar 
+int variavelQueSalvaUmNumero = int.Parse (Console.ReadLine()!);
+
+// verifica se o valor de variavelQueSalveUmNumero é par 
+ if (variavelQueSalvaUmNumero % 2 == 0) 
+  {
+    Console.WriteLine ($"número {variavelQueSalvaUmNumero} é par ");
+  } else {
+    Console.WriteLine ($"número {variavelQueSalvaUmNumero} é impar ");
+  }
